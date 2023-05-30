0807ae2532f9d35bd2c3511e4b6e322fa927d960
Vijaya Manohar Dogiparthi <dvijaymanohar@gmail.com>
2023-05-30 08:50:10 +0200

    Working with Config crate

--- units/cfg_mngt/src/main.rs
+++ units/cfg_mngt/src/main.rs
@@ -1,10 +1,26 @@
+use syslog::{Facility, Formatter3164};
 mod settings;
 
 use settings::Settings;
 
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // Initialize the syslog writer
+    let formatter = Formatter3164 {
+        facility: Facility::LOG_USER,
+        hostname: None,
+        process: "myapp".into(),
+        pid: 0,
+    };
+
+    let mut logger = syslog::unix(formatter)?;
+
+    // Write log messages to Journalctl
+    logger.info("This is an informational message")?;
+    logger.warning("This is a warning message")?;
+
     let settings = Settings::new();
 
     // Print out our settings
-    println!("{:?}", settings);
+    //println!("{:?}", settings);
+    Ok(())
 }
